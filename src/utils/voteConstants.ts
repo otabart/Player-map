24ffbcb887298691666ea73db99ea693f6b86a69
AlexandreTedesco <alexandre.tedesco75@gmail.com>
2025-05-16 21:55:49 +0200

    Update constants for mainnet

--- src/utils/voteConstants.ts
+++ src/utils/voteConstants.ts
@@ -5,17 +5,27 @@ export const UNIT_VALUE =25000000000000n;
 // List of predefined claim IDs for voting
 // Note: These IDs correspond to actual triples in the blockchain
 export const PREDEFINED_CLAIM_IDS = [
-  24596n,
-  24593n,
-  24592n,
-  24580n,
-  24581n,
-  24582n,
-  24575n,
-  24574n,
-  24573n,
-  24572n,
-  24571n,
-  24570n,
-  24569n
+  163985n,
+  163986n,
+  163987n,
+  163988n
 ];
+
+
+// ** TESTNET **
+
+// export const PREDEFINED_CLAIM_IDS = [
+//   24596n,
+//   24593n,
+//   24592n,
+//   24580n,
+//   24581n,
+//   24582n,
+//   24575n,
+//   24574n,
+//   24573n,
+//   24572n,
+//   24571n,
+//   24570n,
+//   24569n
+// ];
