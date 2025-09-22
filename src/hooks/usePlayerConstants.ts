83be52017c3e62d80a8be5f5b8686bc391559e64
AlexandreTedesco <alexandre.tedesco75@gmail.com>
2025-09-22 04:38:48 +0200

    fix re-render

--- src/hooks/usePlayerConstants.ts
+++ src/hooks/usePlayerConstants.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { PlayerMapConfig, PlayerMapConstants, DefaultPlayerMapConstants } from '../types/PlayerMapConfig';
 import { DEFAULT_CONSTANTS, UNIT_VALUE } from '../utils/constants';
 
@@ -7,31 +8,33 @@ import { DEFAULT_CONSTANTS, UNIT_VALUE } from '../utils/constants';
  * @returns Constantes complètes (personnalisées + par défaut)
  */
 export const usePlayerConstants = (config?: PlayerMapConfig): DefaultPlayerMapConstants => {
-  // Si des constantes personnalisées sont fournies, les utiliser
-  if (config?.constants) {
-    console.log('🎯 PlayerMap: Using CUSTOM constants from staging-front', {
-      COMMON_IDS: config.constants.COMMON_IDS,
-      PLAYER_TRIPLE_TYPES: config.constants.PLAYER_TRIPLE_TYPES,
-      OFFICIAL_GUILDS: config.constants.OFFICIAL_GUILDS,
-      PREDEFINED_CLAIM_IDS: config.constants.PREDEFINED_CLAIM_IDS
+  return useMemo(() => {
+    // Si des constantes personnalisées sont fournies, les utiliser
+    if (config?.constants) {
+      console.log('🎯 PlayerMap: Using CUSTOM constants from staging-front', {
+        COMMON_IDS: config.constants.COMMON_IDS,
+        PLAYER_TRIPLE_TYPES: config.constants.PLAYER_TRIPLE_TYPES,
+        OFFICIAL_GUILDS: config.constants.OFFICIAL_GUILDS,
+        PREDEFINED_CLAIM_IDS: config.constants.PREDEFINED_CLAIM_IDS
+      });
+      return {
+        ...config.constants,
+        UNIT_VALUE // Toujours depuis Player-map, jamais paramétrable
+      };
+    }
+    
+    // Sinon, utiliser les constantes par défaut
+    console.log('🔧 PlayerMap: Using DEFAULT constants from Player-map', {
+      COMMON_IDS: DEFAULT_CONSTANTS.COMMON_IDS,
+      PLAYER_TRIPLE_TYPES: DEFAULT_CONSTANTS.PLAYER_TRIPLE_TYPES,
+      OFFICIAL_GUILDS: DEFAULT_CONSTANTS.OFFICIAL_GUILDS,
+      PREDEFINED_CLAIM_IDS: DEFAULT_CONSTANTS.PREDEFINED_CLAIM_IDS
     });
     return {
-      ...config.constants,
-      UNIT_VALUE // Toujours depuis Player-map, jamais paramétrable
+      ...DEFAULT_CONSTANTS,
+      UNIT_VALUE
     };
-  }
-  
-  // Sinon, utiliser les constantes par défaut
-  console.log('🔧 PlayerMap: Using DEFAULT constants from Player-map', {
-    COMMON_IDS: DEFAULT_CONSTANTS.COMMON_IDS,
-    PLAYER_TRIPLE_TYPES: DEFAULT_CONSTANTS.PLAYER_TRIPLE_TYPES,
-    OFFICIAL_GUILDS: DEFAULT_CONSTANTS.OFFICIAL_GUILDS,
-    PREDEFINED_CLAIM_IDS: DEFAULT_CONSTANTS.PREDEFINED_CLAIM_IDS
-  });
-  return {
-    ...DEFAULT_CONSTANTS,
-    UNIT_VALUE
-  };
+  }, [config?.constants]);
 };
 
 /**
